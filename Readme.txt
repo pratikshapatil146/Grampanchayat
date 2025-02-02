50600d093ef6913369ca13ba631480c9b76d6aa9
Pratiksha <pratikshaa6668@gmail.com>
2025-02-02 14:53:12 +0530

    grampanchayat project

--- Readme.txt
+++
@@ -1,1 +0,0 @@
-//this is grampachyat project
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import VillageWebsite from './components/VillageWebsite.jsx';
+
+function App() {
+
+  return (
+    <div>
+      <VillageWebsite />
+    </div>
+  );
+}
+
+export default App;
